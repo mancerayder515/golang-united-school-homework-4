cd0b62ff76b1ab555a54c0eda3a6bea64dad0ae9
Artem Pistryi <gazated@gmail.com>
2022-06-21 12:31:30 +0300

    finally

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,59 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.ReplaceAll(input, " ", "")
+	
+	if len(input) == 0 {
+		err = fmt.Errorf("input is not valid: %w", errorEmptyInput)
+		return
+	}
+	
+	strNums := Split(
+		input, []rune{'+', '-'})
+	
+	var nums []int
+	for _, s := range strNums {
+		n, ok := strconv.Atoi(s)
+		if ok != nil {
+			err = fmt.Errorf("inappropriate symbols: %w", ok)
+			return
+		}
+		nums = append(nums, n)
+	}
+	
+	if len(strNums) != 2 {
+		err = fmt.Errorf("invalid input: %w", errorNotTwoOperands)
+		return
+	}
+	
+	var sign = 1
+	var numToAdd int8 = 0
+	var sum int
+	
+	for i := 0; i < len(input); i++ {
+		if input[i] == '-' {
+			sign *= -1
+		} else if input[i] == '+' {
+			continue
+		} else {
+			i += len(strNums[numToAdd]) - 1
+			sum += nums[numToAdd] * sign
+			numToAdd++
+			sign = 1
+		}
+	}
+	output = strconv.Itoa(sum)
+	return
+}
+
+func Split(str string, separators []rune) []string {
+	f := func(r rune) bool {
+		for _, s := range separators {
+			if r == s {
+				return true
+			}
+		}
+		return false
+	}
+	return strings.FieldsFunc(str, f)
 }
